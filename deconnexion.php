b802827587e2685d3189f097287b3c07358f5e53
RomainElbaamrani <romainelbaamrani@gmail.com>
2020-04-19 17:04:34 +0200

    modification de index et deconnexion

--- deconnexion.php
+++ deconnexion.php
@@ -3,5 +3,5 @@ session_start();
 session_destroy();
 // si le bouton deconnexion est cliqué, stoper la session et retourner à la page index
 
-header('Location: index.php');
+header('Location: index.html');
 ?>
